82d642f384fefa766846bcdbe0f444fb2d804db0
bharathyahoo <bharath_j@yahoo.com>
2025-09-26 16:12:04 +0530

    Code Review Comments from Code Rabbit Implemented

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log(`Visit http://localhost:${PORT} to see the Hello World message`);
-});
-
+// Export the app for testing or importing in other files
 module.exports = app;
+
+// Start the server only when this file is run directly
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Visit http://localhost:${PORT} to see the Hello World message`);
+  });
+}
